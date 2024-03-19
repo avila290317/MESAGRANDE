d0775fd47b72af7fe7a415b71b7699138b354d14
Natalia Avila <2036000855@utna.edu.mx>
2024-03-19 11:41:17 -0600

    Estadia

---
+++ api/sendmail.js
@@ -0,0 +1,50 @@
+document.addEventListener('submit', sendMail);
+
+function sendMail(e) {
+  e.preventDefault();
+  let name = document.getElementById('name').value;
+  let phone = document.getElementById('phone').value;
+  let mail = document.getElementById('email').value;
+  let message = document.getElementById('mensaje').value;
+  var validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+  if (name == '') {
+    alert('Te falta agregar un nombre');
+    return;
+  } else if (mail == '') {
+    alert('Te falta agregar un correo');
+    return;
+  } else if (phone == '') {
+    alert('Te falta agregar un telefono');
+    return;
+  } else if (message == '') {
+    alert('Te falta agregar un mensaje');
+    return;
+  } else if (validEmail.test(mail)) {
+
+    let _datos = {
+      name: name,
+      phone: phone,
+      mail: mail,
+      message:message
+    }
+
+    fetch('http://127.0.0.1:3000/sendmail', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(_datos)
+    })
+      .then(response => response.text())
+      .then(res => {
+        console.log(res);
+        alert("Se envió el correo, gracias!!!");
+        document.getElementById('form').reset();
+      })
+      .catch(err => console.log(err));
+  } else {
+    console.log('Correo no valido');
+    alert('Correo no valido');
+    return;}
+}
---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+var cors = require('cors');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('public')); 
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Configura nodemailer con tus detalles SMTP
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'ahnatalia25@gmail.com',
+    pass: 'ofpiqbvozxznzqyt', 
+  },
+});
+
+// Ruta para manejar el envío de correos electrónicos
+app.post('/sendmail', (req, res) => {
+  const { name, phone, mail, message } = req.body;
+
+  const mailOptions = {
+    from: `${mail}`,
+    to: 'ahnatalia25@gmail.com', 
+    subject: 'Correo Electrónico desde la página web',
+    text: `Nombre: ${name}\nTelefono: ${phone}\nEmail: ${mail}\nMensaje: ${message}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Correo enviado: ' + info.response);
+  });
+});
+
+app.post('/sendJobMail', (req, res) => {
+  const { nombre, email, telefono, direccion, experiencia, educacion, habilidades, proyecto } = req.body;
+  let emailAEnviar = "";
+  if (proyecto == "calvillo") {
+    emailAEnviar = "avilahernandeznatalia123@gmail.com";
+  } else if (proyecto == "california") {
+    emailAEnviar = "ahnatalia@gmail.com";
+  }
+
+  const mailOptions = {
+    from: `${email}`,
+    to: emailAEnviar, 
+    subject: 'Solicitud de empleo',
+    text: `Nombre: ${nombre}\nTelefono: ${telefono}\nEmail: ${email}\nDireccion: ${direccion}\nExperiencia: ${experiencia}\nEducacion: ${educacion}\nHabilidades: ${habilidades}\nProyecto: ${proyecto}`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return res.status(500).send(error.toString());
+    }
+    res.status(200).send('Correo enviado: ' + info.response);
+  });
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`Proyecto Invernaderos Mesa Grande esta escuchando en el http://localhost:${port}`)
+});
