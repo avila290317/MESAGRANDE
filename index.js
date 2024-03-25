444cdb2545e078901efaa908978c21b7a5dcfb41
Carlos Galvan <danielgalvanastorga@gmail.com>
2024-03-25 15:47:31 -0600

    port

--- index.js
+++ index.js
@@ -5,22 +5,24 @@ const bodyParser = require('body-parser');
 var cors = require('cors');
 
 const app = express();
-const port = 3000;
+
 
 // app.use(express.static('public')); 
 
-app.use(cors());
+
 // app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.json());
 
 app.use(express.json())
 //decodifica la informacion y la convierte en formato json//
 app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
 
 
 app.use("/mesagrande", router)
 // Inicia el servidor
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Proyecto Invernaderos Mesa Grande esta escuchando en el http://localhost:${port}`)
 });
